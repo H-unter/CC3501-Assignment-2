2704d93f761f9617864b693991420b4ffa6119ad
qbouet <quentin.bouet@my.jcu.edu.au>
2024-09-05 10:08:37 +1000

    add stuff to derlete

---
+++ code/SDI_12_test/main.c
@@ -0,0 +1,17 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 05-Sep-2024 cheva
+ * SDI_12_test
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+#include "hardware/pwm.h"
+#include "hardware/uart.h"
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
