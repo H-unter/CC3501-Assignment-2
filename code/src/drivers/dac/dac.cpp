0699b2278c5b67cd33b34ad8b3aeeae1c3871b8d
qbouet <quentin.bouet@my.jcu.edu.au>
2024-09-30 15:36:33 +1000

    Add DAC class

---
+++ code/src/drivers/dac/dac.cpp
@@ -0,0 +1,15 @@
+#include "dac.h"
+
+// Constructor
+DAC::DAC() {
+}
+
+// Initialise DAC
+void DAC::init() {
+
+}
+
+// Set DAC voltage
+void DAC::set_voltage() {
+
+}
---
+++ code/src/drivers/dac/dac.h
@@ -0,0 +1,16 @@
+#ifndef DAC_H
+#define DAC_H
+
+class DAC {
+public:
+    // Constructor
+    DAC();
+
+    // Method to initialise DAC
+    void init();
+
+    // Method to set DAC voltage
+    void set_voltage();
+};
+
+#endif // DAC_H
